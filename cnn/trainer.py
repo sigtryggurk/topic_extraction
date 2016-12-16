fa45b244d15f2d48899912fd861112cc2755fd82
Siggi <sigtryggurk@gmail.com>
2016-12-16 02:08:24 -0800

    Add predict option

--- cnn/trainer.py
+++ cnn/trainer.py
@@ -64,16 +64,40 @@ def test(model, tokenizer):
   
   # Pad/Cut sequences at the maximum length 
   inputs = pad_sequences(inputs, maxlen=cfg.MAX_SEQUENCE_LENGTH)
-
+  
   return model.evaluate(inputs, targets, batch_size=cfg.BATCH_SIZE)
+
+def predict(model, tokenizer):
+  print("Predicting on test data")
+  # Load the test data
+  texts, targets = loadTextsAndLabels(cfg.TEST_DATA_PATH)
+  
+  # Vectorize the text samples into a 2D integer tensor
+  inputs = tokenizer.texts_to_sequences(texts)
+  
+  # Pad/Cut sequences at the maximum length 
+  inputs = pad_sequences(inputs, maxlen=cfg.MAX_SEQUENCE_LENGTH)
+ 
+  preds = np.rint(model.predict(inputs, batch_size=cfg.BATCH_SIZE))
+  correct = 0
+  for i, target in enumerate(targets):
+    if np.equal(preds[i], target):
+      correct += 1
+  print(correct,len(targets))
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("-r","--restore", default=None, type=str, help="Specify the run you'd like to restore from. Set to 'latest' if you want restore the latest run.")
-  parser.add_argument("-t","--train", action="store_true", default=True, help="if true, trains the model on the training set")
-  parser.add_argument("-e","--test", action="store_true", default=True, help="if true, evaluates the current model on the test set")
+  parser.add_argument("-t","--train", dest="train", action="store_true", help="train the model on the training set")
+  parser.add_argument("-nt","--no-train", dest="train", action="store_false", help="don't train the model on the training set")
+  parser.add_argument("-e","--test", dest="test", action="store_true", help="evaluate the model on the test set")
+  parser.add_argument("-ne","--no-test", dest="test", action="store_false", help="don't evaluate the model on the test set")
+  parser.add_argument("-p","--predict", dest="predict", action="store_true", help="predict on the test set")
+  parser.add_argument("-np","--no-predict", action="store_false", default=False, help="don't predict on the test set")
+  parser.set_defaults(train=True, test=True, predict=False)  
 
   args = parser.parse_args()
+ 
 
   model = tokenizer = None
   if args.restore != None:
@@ -97,3 +121,7 @@ if __name__ == '__main__':
   if args.test and model != None and tokenizer != None:
     scores = test(model, tokenizer)
     print(" - ".join(["%s: %.4f" % (model.metrics_names[i], score) for i,score in enumerate(scores)]))
+
+  if args.predict and model != None and tokenizer != None:
+    predict(model, tokenizer)
+
